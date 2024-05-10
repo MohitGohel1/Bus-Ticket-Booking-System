8459a0eae4c609c946e8cb41397870ba4ab5e42e
MohitGohel1 <123924231+MohitGohel1@users.noreply.github.com>
2024-05-10 10:30:47 +0530

    Add files via upload

---
+++ JdbcConstant.java
@@ -0,0 +1,8 @@
+package com.constant;
+
+public interface JdbcConstant {
+
+	public static final String url="jdbc:mysql://localhost:3306/busticketreservation";
+	public static final String username="root";
+	public static final String password="961336@mM";
+}
